127240d76e69bdf834948ba4b6c219c269883a0e
BatogovRoman <romanbatogov@gmail.com>
2025-10-26 14:34:37 +0500

    Task2

---
+++ Tyuiu.BatogovRK.Sprint3.Task2.V15.Test/DataServiceTest.cs
@@ -0,0 +1,22 @@
+﻿using Tyuiu.BatogovRK.Sprint3.Task2.V15.Lib;
+
+namespace Tyuiu.BatogovRK.Sprint3.Task2.V15.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+
+            double a = 0.5;
+            int Start = 1;
+            int End = 20;
+            double wait = 0.298;
+
+            double res = ds.GetSumSeries(a, Start, End);
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.BatogovRK.Sprint3.Task2.V15/Program.cs
@@ -0,0 +1,35 @@
+﻿using Tyuiu.BatogovRK.Sprint3.Task2.V15.Lib;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+        Console.Title = "Спринт #3 | Выполнил: Батогов Р. К. | АСОиУб-25-1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #3                                                               *");
+        Console.WriteLine("* Тема: Оператор цикла while                                              *");
+        Console.WriteLine("* Задание #2                                                              *");
+        Console.WriteLine("* Вариант #15                                                             *");
+        Console.WriteLine("* Выполнил: Батогов Роман Константинович | АСОиУб-25-1                    *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Написать программу используя цикл do...while, которая вычисляет сумму   *");
+        Console.WriteLine("* ряда по формуле, при a=0,5                                              *");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+        double a = 0.5;
+        int start = 1;
+        int stop = 20;
+        Console.WriteLine("* Переменная A = " + a);
+        Console.WriteLine("* Старт шага = " + start);
+        Console.WriteLine("* Конец шага = " + stop);
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+        double res = ds.GetSumSeries(a, start, stop);
+        Console.WriteLine(res);
+    }
+}
