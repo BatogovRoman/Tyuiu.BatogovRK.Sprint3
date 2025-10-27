8177895a8fce5ccc4016824a690b954ba1a0c263
BatogovRoman <romanbatogov@gmail.com>
2025-10-27 12:58:22 +0500

    Task4

---
+++ Tyuiu.BatogovRK.Sprint3.Task4.V23.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿
+using Tyuiu.BatogovRK.Sprint3.Task4.V23.Lib;
+
+namespace Tyuiu.BatogovRK.Sprint3.Task4.V23.Test
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new();
+            int x = -5, y = 5;
+            var res = ds.Calculate(x, y);
+            Assert.AreEqual(244.656, res);
+        }
+    }
+}
---
+++ Tyuiu.BatogovRK.Sprint3.Task4.V23/Program.cs
@@ -0,0 +1,45 @@
+﻿using Tyuiu.BatogovRK.Sprint3.Task4.V23.Lib;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new();
+
+        Console.Title = "Спринт #3 | Выполнил: Батогов Р. К. | АСОиУб-25-1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #3                                                               *");
+        Console.WriteLine("* Тема: Использование операторов continue и break в циклах                *");
+        Console.WriteLine("* Задание #4                                                              *");
+        Console.WriteLine("* Вариант #23                                                             *");
+        Console.WriteLine("* Выполнил: Батогов Роман Константинович | АСОиУб-25-1                    *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* На отрезке, где x принимает значения от -5 до 5, вычислить значение     *");
+        Console.WriteLine("* функции y=(cos(x)/x)+3. При х = 0 прервать цикл.                        *");
+        Console.WriteLine("* Полученные значения перемножать.                                        *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+
+        int x, y;
+
+        do
+        {
+            Console.WriteLine("Введите начальное значение:");
+            x = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введите конечное значение:");
+            y = Convert.ToInt32(Console.ReadLine());
+
+            if (x > y) Console.WriteLine("Начальное значение не может быть больше конечного, попробуйте ещё раз.");
+        } while (x > y);
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+        Console.WriteLine("Результат произведения: " + ds.Calculate(x, y));
+        Console.ReadLine();
+    }
+}
