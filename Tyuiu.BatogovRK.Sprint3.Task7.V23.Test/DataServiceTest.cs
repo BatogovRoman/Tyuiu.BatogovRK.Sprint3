f7d516f077d6ebd02567507c62451c2341e84ae2
BatogovRoman <romanbatogov@gmail.com>
2025-10-27 14:13:14 +0500

    Task7

---
+++ Tyuiu.BatogovRK.Sprint3.Task7.V23.Test/DataServiceTest.cs
@@ -0,0 +1,34 @@
+﻿using Tyuiu.BatogovRK.Sprint3.Task7.v23.Lib;
+
+namespace Tyuiu.BatogovRK.Sprint3.Task7.V23.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            int startvalue = -5;
+            int stopvalue = 5;
+            int len = stopvalue - startvalue + 1;
+            double[] valuewaitarray;
+            valuewaitarray = new double[len];
+            valuewaitarray[0] = 13.81;
+            valuewaitarray[1] = 11.87;
+            valuewaitarray[2] = 9.87;
+            valuewaitarray[3] = 7.74;
+            valuewaitarray[4] = 5.36;
+            valuewaitarray[5] = 2.50;
+            valuewaitarray[6] = 0;
+            valuewaitarray[7] = 0.79;
+            valuewaitarray[8] = -1.75;
+            valuewaitarray[9] = -3.78;
+            valuewaitarray[10] = -5.71;
+            double[] res;
+            res = new double[len];
+            res = ds.GetMassFunction(startvalue, stopvalue);
+            CollectionAssert.AreEqual(res, valuewaitarray);
+        }
+    }
+}
---
+++ Tyuiu.BatogovRK.Sprint3.Task7.V23/Program.cs
@@ -0,0 +1,47 @@
+﻿using Tyuiu.BatogovRK.Sprint3.Task7.v23.Lib;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+        Console.Title = "Спринт #3 | Выполнил: Батогов Р. К. | АСОиУб-25-1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #3                                                               *");
+        Console.WriteLine("* Тема: Добавление к решению итоговых проектов по спринту.                *");
+        Console.WriteLine("* Задание #7                                                              *");
+        Console.WriteLine("* Вариант #23                                                             *");
+        Console.WriteLine("* Выполнил: Батогов Роман Константинович | АСОиУб-25-1                    *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+        int startvalue = -5;
+        int stopvalue = 5;
+
+        Console.WriteLine("Старт шага " + startvalue);
+
+        Console.WriteLine("Конец шага " + stopvalue);
+
+        int len = ds.GetMassFunction(startvalue, stopvalue).Length;
+        double[] valuearray;
+        valuearray = new double[len];
+
+        valuearray = ds.GetMassFunction(startvalue, stopvalue);
+
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+        Console.WriteLine("+----------+----------+");
+        Console.WriteLine("|    X     |   f(x)   |");
+        Console.WriteLine("+----------+----------+");
+        for (int i = 0; i<=len-1; i++)
+        {
+            Console.WriteLine("|{0,5:d}     |  {1,5:f2}   |", startvalue, valuearray[i]);
+            startvalue++;
+        }
+        Console.WriteLine("+----------+----------+");
+        Console.ReadKey();
+    }
+}
